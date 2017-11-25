928de35ba37b97c3742621230382423d4922606d
xxDOOMbox <shawwian@gmail.com>
2017-11-24 19:08:04 -0500

    save choreography to backend feature

--- src/containers/ChoreographyContainer.js
+++ src/containers/ChoreographyContainer.js
@@ -2,14 +2,23 @@ import React from 'react';
 import PoseList from '../components/PoseList';
 import { removePose } from '../actions/poses'; 
 import { bindActionCreators } from 'redux'; 
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { saveChoreography } from '../services/choreoBackendApi'; 
 
 class ChoreographyContainer extends React.Component {
+
+
+  handleSave = () => {
+    // make a post request to the backend 
+    // HARD CODED USER_ID AS ONE CURRENTLY!!!
+    saveChoreography(1, this.props.poses)
+  }
 
   render() {
     return (
       <div>
         <PoseList poses={this.props.poses} removePose={this.props.removePose}/>
+        {this.props.poses.length > 0 ? <button onClick={this.handleSave}>Save</button> : null }
       </div>
     )
   }
--- src/services/choreoBackendApi.js
+++ src/services/choreoBackendApi.js
@@ -1,7 +1,24 @@
+const baseURL = 'http://localhost:3000/api/v1'
+
 export function login(params) {
-  return fetch('https://localhost:3000/login', {
+  return fetch(`${baseURL}/login`, {
     method: 'POST', 
     body: JSON.stringify(params),
   }).then(res => res.json())
+}
+
+export function saveChoreography(id, choreo) {
+  const body = {user_id: id, poses_json: JSON.stringify(choreo)};
+
+  return fetch(`${baseURL}/choreographies`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      Accepts: 'application/json',
+      'Content-Type': 'application/json'
+    }
+    // JWT TOKEN / AUTH INFO 
+  }).then(res => res.json())
+  .then(json => console.log(json)); 
 }
 
