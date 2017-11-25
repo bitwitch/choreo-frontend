d1e1b1536308d375713649e9d1b7dd8c8783543d
xxDOOMbox <shawwian@gmail.com>
2017-11-24 20:17:40 -0500

    prototype choreo playback

--- src/containers/FigureContainer.js
+++ src/containers/FigureContainer.js
@@ -5,7 +5,7 @@ import { addPose, setCurrentPose } from '../actions/poses';
 import { bindActionCreators } from 'redux'; 
 import { connect } from 'react-redux'; 
 import { Line, Ellipse } from 'react-konva'; 
-
+import './FigureContainer.css';
 class FigureContainer extends React.Component {
 
   onAddPose = () => {
@@ -29,7 +29,7 @@ class FigureContainer extends React.Component {
 
   render() {
     return (
-      <div> 
+      <div className='figure-container'> 
         <Figure setCurrentPose={this.props.setCurrentPose} />
         <button onClick={this.onAddPose}>Add Pose</button>
         <button onClick={this.props.reset}>Reset</button>
--- src/containers/PlaybackContainer.js
+++ src/containers/PlaybackContainer.js
@@ -1,8 +1,30 @@
 import React from 'react'; 
 import { connect } from 'react-redux'; 
-import { Line, Ellipse } from 'react-konva'; 
+import { Line, Ellipse, Stage, Layer } from 'react-konva'; 
+import './PlaybackContainer.css'; 
 
 class PlaybackContainer extends React.Component {
+  state = {
+    frames: [],
+    frameCounter: 0
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.poses.list.length !== this.props.poses.list.length) {
+      const frames = nextProps.poses.list.map(pose => {
+        return this.resizePose(pose.lines); 
+      });
+
+      this.setState({
+        frames
+      });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.frameCounter !== this.state.frameCounter) return true;
+    return nextProps.poses.list.length === this.props.poses.list.length ? false : true;
+  }
 
   resizePose = (lines) => {
     let i = 0;
@@ -18,13 +40,37 @@ class PlaybackContainer extends React.Component {
     });
   }
 
+  handlePlay = () => {
+    this.play = setInterval(() => {
+      const newFrameCount = this.state.frameCounter === this.state.frames.length - 1 ? 0 : this.state.frameCounter + 1;
+      this.setState({frameCounter: newFrameCount})
+    }, 300)
+  }
+
+  handleStop = () => {
+    clearInterval(this.play);
+    this.setState({frameCounter: 0})
+  }
+
+  handlePause = () => {
+    clearInterval(this.play);
+  }
+
   render() {
+    const currentFrame = this.state.frames[this.state.frameCounter];
+    console.log(currentFrame)
+
     return (
       <div className="playback-container"> 
-
-        <button>Play</button>
-        <button>Pause</button>
-        <button>Stop</button>
+        frame: {this.state.frameCounter}
+        <Stage width={300} height={300}>
+          <Layer>
+            {currentFrame}
+          </Layer> 
+        </Stage>
+        <button onClick={this.handlePlay}>Play</button>
+        <button onClick={this.handlePause}>Pause</button>
+        <button onClick={this.handleStop}>Stop</button>
       </div> 
     )
   }
