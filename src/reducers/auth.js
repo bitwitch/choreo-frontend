7f98d9455274e3227e51048cfa77e754f4be6100
xxDOOMbox <shawwian@gmail.com>
2017-11-30 13:11:26 -0500

    styling welcome page

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,4 +1,18 @@
-export default function authReducer(state={}, action) {
+const defaultState = {
+  user: {
+    info: {
+      photo: {url: null},
+      name: null,
+      age: null, 
+      location: null
+    },
+    friends: [],
+    likes: [], 
+    choreographies: []
+  }
+} 
+
+export default function authReducer(state=defaultState, action) {
   switch(action.type) {
     case 'LOGIN_USER': 
       return {...state, ...action.payload}
