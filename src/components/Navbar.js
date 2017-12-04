ce94c2a9e89307cfcaaf8743ae72f3173f79d761
xxDOOMbox <shawwian@gmail.com>
2017-12-04 18:00:01 -0500

    add in browser player buttons (play, pause, stop)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,44 @@
-import React from 'react'; 
-import { NavLink } from 'react-router-dom'; 
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 import '../style/NavBar.css'
 
 class Navbar extends React.Component {
+  state = {
+    playing: false
+  }
 
   handleClick = (e) => {
     e.preventDefault()
     this.props.logout()
   }
 
+  handlePause = () => {
+    this.props.player.togglePlay() 
+    this.setState({playing: false})
+  }
+
+  handlePlay = () => {
+    this.props.player.togglePlay()
+    this.setState({playing: true})
+  }
+
+  handleStop = () => {
+    this.props.player.togglePlay()
+    this.props.player.seek(0)
+    this.setState({playing: false})
+  }
+
   render() {
+
+    if (this.props.player) console.log(this.props.player)
+
+    const playPauseButton = (this.state.playing) ? 
+      <button onClick={this.handlePause}><img height={20} width={20} src='https://i.imgur.com/Qo6uE4L.png' alt='Pause'/></button>
+    :
+      <button onClick={this.handlePlay}><img height={20} width={20} src='https://i.imgur.com/NTz3SDu.png' alt='Play'/></button>
+    ;
+
     return (
       <div className='nav'>
         <ul>
@@ -18,18 +47,31 @@ class Navbar extends React.Component {
           <li><NavLink to='/trending'>Trending</NavLink></li>
           <li id='title'>Shaker Maker</li>
           <li id='logout'><NavLink onClick={this.handleClick} to='/logout'>Logout</NavLink></li>
-
-          <li id='player'>
-            <div>
-              <button>Play</button>
-              <button>Stop</button>
-            </div>
-          </li>
-
+          {this.props.player ? 
+            <li id='player'>
+              <div>
+                {playPauseButton}
+                <button onClick={this.handleStop}><img height={20} width={20} src='https://i.imgur.com/unx98ZO.png' alt='Stop'/></button>
+              </div>
+            </li>
+          : null}
         </ul> 
       </div>
     )
   }
-}; 
+}
 
-export default Navbar;
+function mapStateToProps(state) {
+  return {
+    player: state.player 
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
+
+
+
+
+
+
+
